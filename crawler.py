0cffdb9d3d6b9a35a939966caf8b65be452ad2bd
litalshl <lital.shalev@gmail.com>
2022-08-26 17:05:59 +0300

    Break the code to async functions

--- crawler.py
+++ crawler.py
@@ -1,3 +1,4 @@
+from typing import Collection
 import requests
 from lxml import etree
 from pymongo import MongoClient
@@ -8,63 +9,84 @@ import os
 import time
 import asyncio
 
-load_dotenv()
+async def main():
+    load_dotenv()
 
-client = MongoClient(os.getenv("MONGODB_HOST"), int(os.getenv("MONGODB_PORT")))
+    client = MongoClient(os.getenv("MONGODB_HOST"), int(os.getenv("MONGODB_PORT")))
 
-db = client.crawler_db
-result = db.websites.create_index(('url'), unique=True)
-websites = db.websites
+    db = client.crawler_db
+    db.websites.create_index(('url'), unique=True)
+    websites = db.websites
 
-df = pd.read_csv(os.getenv("CSV_FILE"))  
-for index, row in df.iterrows():    
-    try:
-        websites.update_one({'url': row["website_host"]}, 
-        {"$set":{
-            "numPagesToDownload" : row[" number_of_pages_to_download"],        
-            "maxConcurrentConnections" : int(os.getenv("MAX_CONCURRENT_CONNECTIONS_WEBSITE")),
-            "downloadedPages" : 0,
-            "dom" : "",
-            "urls" : [],
-            "pages": []
-        }}, upsert=True)
+    await data_seeder(websites)
+
+    # TODO: extract to a consumer class that gets the url from the message
+    URL = "https://www.vortex.com/"
+    retries = 0
+    try:        
+        dom = await download_page(URL, retries, client) 
+        await scrape_page(URL, dom, client)
     except:
-        print("Website was not added to DB, error: ", sys.exc_info()[0]) 
+        if retries <= int(os.getenv("MAX_DOWNLOAD_RETRIES")):
+            asyncio.sleep(int(os.getenv("DOWNLOAD_RETRIES_INTERVAL_SEC")))
+            await download_page(URL, retries, client) 
+            retries+= 1
 
-# Downloader and scrapper basic functionality
-URL = "https://www.vortex.com/"
-retries = 0
-try:        
-    resp = requests.get(URL)
+async def data_seeder(websites : Collection):
+    df = pd.read_csv(os.getenv("CSV_FILE"))  
+    for index, row in df.iterrows():    
+        try:
+            websites.update_one({'url': row["website_host"]}, 
+            {"$set":{
+                "numPagesToDownload" : row[" number_of_pages_to_download"],        
+                "maxConcurrentConnections" : int(os.getenv("MAX_CONCURRENT_CONNECTIONS_WEBSITE")),
+                "downloadedPages" : 0,
+                "dom" : "",
+                "urls" : [],
+                "pages": []
+            }}, upsert=True)
+        except:
+            print("Website was not added to DB, error: ", sys.exc_info()[0]) 
+
+async def download_page(url : str, retries : int, db_client: MongoClient):
+    resp = requests.get(url)
     if resp.status_code == 200:
         # Create DOM from HTML text
         dom = etree.HTML(resp.text)
-        websiteDoc = client.crawler_db.websites.find_one({'url': URL})
-        websiteDoc["dom"] = dom
-        # Search for the <a> element and get the href, check if is a subpage of the original website
-        for elt in dom.xpath('//a'):
-            if URL in elt.attrib['href']:
-                print(elt.attrib['href'])
-                # if url doesn't already exist in the DB, add it to the relevant website document
-                page = {
-                    "url" : elt.attrib['href'],
-                    "dom" : ""
-                }
-                websiteDoc["urls"].append(page["url"])
-                websiteDoc["pages"].append(page)
-                try:
-                    client.db.collection.update_one({'url': URL}, 
-                    {"$set":{
-                        "dom" : str(websiteDoc["dom"]),
-                        "urls" : websiteDoc["urls"], 
-                        "pages" : websiteDoc["pages"]}})
-                except:
-                    print("Website document was not updated in DB, error: ", sys.exc_info()[0])
-except:
-    if retries <= int(os.getenv("MAX_DOWNLOAD_RETRIES")):
-        asyncio.sleep(int(os.getenv("DOWNLOAD_RETRIES_INTERVAL_SEC")))
-        # scrap(URL, retries)
-        retries+= 1
+        try:
+            db_client.db.collection.update_one({'url': url}, 
+            {"$set":{
+                "dom" : str(dom)
+            }})
+        except:
+            print("Website document was not updated in DB, error: ", sys.exc_info()[0])
+        return dom
+        
+async def scrape_page(url : str, dom : str, db_client : MongoClient):
+    # Search for the <a> element and get the href, check if is a subpage of the original website
+    for elt in dom.xpath('//a'):
+        if url in elt.attrib['href']:
+            print(elt.attrib['href'])
+            # if url doesn't already exist in the DB, add it to the relevant website document
+            page = {
+                "url" : elt.attrib['href'],
+                "dom" : ""
+            }
+            await store_page_url_in_db(url, db_client, page)
+                
+async def store_page_url_in_db(url : str, db_client : MongoClient, page : object):
+    websiteDoc = db_client.crawler_db.websites.find_one({'url': url})
+    websiteDoc["urls"].append(page["url"])
+    websiteDoc["pages"].append(page)
+    try:
+        db_client.db.collection.update_one({'url': url}, 
+        {"$set":{
+            "urls" : websiteDoc["urls"], 
+            "pages" : websiteDoc["pages"]}})
+    except:
+        print("Website document was not updated in DB, error: ", sys.exc_info()[0])
+
+asyncio.run(main())
 
 
 # Tests to perform:
